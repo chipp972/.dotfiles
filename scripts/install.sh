1a084f1975e6bcd9a78a818686ee8965e341f96d
Pierre-Charles Nicolas <pierrecharles.nicolas@gmail.com>
2016-02-26 15:31:49 +0100

    install in another file

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,53 +2,31 @@
 
 CONFIG_HOME=$HOME/.dotfiles
 
-# install the softwares
-sudo apt-get install make cmake tig tmux zsh python npm
-sudo apt-get install python-dev python-pip python3-dev python3-pip python2.7
+# create the symlinks
+"$CONFIG_HOME"/scripts/link.sh
+
+
+# install softwares (need sudo)
+NO_INSTALL=$1
+if [ ! "$NO_INSTALL" ]; then
+	"$CONFIG_HOME"/scripts/software_install.sh
+fi
 
 # install prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
 
-# install nvim
-IS_UBUNTU=$(uname -a | grep ubuntu -i)
-NEOVIM_INSTALLED=$(command -v nvim)
-if [ "$IS_UBUNTU" -a ! "$NEOVIM_INSTALLED" ]; then
-  sudo apt-get install software-properties-common
-  sudo add-apt-repository ppa:neovim-ppa/unstable
-  sudo apt-get update
-  sudo apt-get install neovim
-  sudo pip3 install neovim
-fi
-
-# alias vim and vi to nvim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+# install fuzzy finder
+git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME"/.fzf
+"$HOME"/.fzf/install
 
 # install vim-plug
 curl -fLo "$CONFIG_HOME"/nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# create the symlinks
-"$CONFIG_HOME"/scripts/link.sh
-
-# change default shell to zsh
-chsh -s /bin/zsh
-
-
-# additionals softwares
-## languages
-sudo apt-get install golang markdown gcc
-## linters
-sudo apt-get install shellcheck pylint
-sudo npm install jsonlint -g
-# install fuzzy finder
-git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME"/.fzf
-"$HOME"/.fzf/install
-
-# upgradeand update packages
-sudo apt-get upgrade
-sudo apt-get update
+# vm configurations
+if [ "$IS_VM" ]; then
+	"$CONFIG_HOME"/scripts/vm_install.sh
+fi
 
 # end message
 echo "Installation done"
---
+++ scripts/software_install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# install the softwares
+sudo apt-get install make cmake tig tmux zsh python npm
+sudo apt-get install python-dev python-pip python3-dev python3-pip python2.7
+
+# install nvim
+IS_UBUNTU=$(uname -a | grep ubuntu -i)
+NEOVIM_INSTALLED=$(command -v nvim)
+if [ "$IS_UBUNTU" -a ! "$NEOVIM_INSTALLED" ]; then
+  sudo apt-get install software-properties-common
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update
+  sudo apt-get install neovim
+  sudo pip3 install neovim
+fi
+
+# alias vim and vi to nvim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+
+# additionals softwares
+## languages
+sudo apt-get install golang markdown gcc
+## linters
+sudo apt-get install shellcheck pylint
+sudo npm install jsonlint -g
+
+# change default shell to zsh
+chsh -s /bin/zsh
+
+# upgrade and update packages
+sudo apt-get upgrade
+sudo apt-get update
+
+
