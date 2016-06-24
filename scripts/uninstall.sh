46e427d57d6f583444f07e250c56b1863464e93b
Pierre-Charles Nicolas <pierrecharles.nicolas@gmail.com>
2016-06-24 10:07:19 +0200

    uninstall script fix

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+rm -rf $HOME/.zprezto
 rm -f $HOME/.z*
 rm -f $HOME/.gitconfig
 rm -f $HOME/.tmux.conf
-rm -rf $HOME/.zprezto
 
-$HOME/.fzf/uninstall
 rm -rf $HOME/.fzf
-
+$HOME/.fzf/uninstall
